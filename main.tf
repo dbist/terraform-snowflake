0e9d6418beae05493a1b33a3f766dbdd5682a2cf
Artem Ervits <generic13@gmail.com>
2024-09-26 14:36:06 -0400

    updated provider

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.95"
+      version = "~> 0.96"
     }
   }
 }
@@ -20,18 +20,22 @@ resource "snowflake_schema" "schema" {
   database = "TF_DEMO"
 }
 
-resource "snowflake_stage" "example_stage" {
-  name        = "TF_DEMO_STAGE"
-  database    = "TF_DEMO"
-  schema      = "TERRAFORM"
-}
+# assumes the schema exists
 
-resource "snowflake_network_rule" "rule" {
-  name       = "internal_stage_rule"
-  database   = "TF_DEMO"
-  schema     = "TERRAFORM"
-  comment    = "A network rule for internal stage."
-  type       = "AWSVPCEID"
-  mode       = "INTERNAL_STAGE"
-  value_list = ["vpce-xxxxxx"]
-}
+#resource "snowflake_stage" "example_stage" {
+#  name        = "TF_DEMO_STAGE"
+#  database    = "TF_DEMO"
+#  schema      = "TERRAFORM"
+#}
+
+# assumes stage exists, requires a valid vpc
+
+#resource "snowflake_network_rule" "rule" {
+#  name       = "internal_stage_rule"
+#  database   = "TF_DEMO"
+#  schema     = "TERRAFORM"
+#  comment    = "A network rule for internal stage."
+#  type       = "AWSVPCEID"
+#  mode       = "INTERNAL_STAGE"
+#  value_list = ["vpce-xxxxxx"]
+#}
