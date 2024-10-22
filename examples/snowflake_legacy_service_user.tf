fef8f1713a820c9c2ba64fe3a8e6fb0a7ccfb7da
Artem Ervits <generic13@gmail.com>
2024-10-22 12:24:40 -0400

    added legacy service user example

---
+++ examples/snowflake_legacy_service_user.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.97"
+    }
+  }
+}
+
+provider "snowflake" {
+  role = "SECURITYADMIN"
+}
+
+resource "snowflake_service_user" "service_user" {
+  name         = "Snowflake Service User"
+  login_name   = "service_user"
+  email        = "service_user@snowflake.example"
+
+  rsa_public_key   = <<EOT
+  <RSA_PUBLIC_KEY_HERE>
+EOT
+}
+
+resource "snowflake_legacy_service_user" "legacy_service_user" {
+  name         = "Snowflake Legacy Service User"
+  login_name   = "legacy_service_user"
+  email        = "legacy_service_user@snowflake.example"
+
+  rsa_public_key   = <<EOT
+  <RSA_PUBLIC_KEY_HERE>
+EOT
+}
--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.96"
+      version = "~> 0.97"
     }
   }
 }
@@ -10,32 +10,3 @@ terraform {
 provider "snowflake" {
   role = "SYSADMIN"
 }
-
-resource "snowflake_database" "db" {
-  name = "TF_DEMO"
-}
-
-resource "snowflake_schema" "schema" {
-  name     = "TERRAFORM"
-  database = "TF_DEMO"
-}
-
-# assumes the schema exists
-
-#resource "snowflake_stage" "example_stage" {
-#  name        = "TF_DEMO_STAGE"
-#  database    = "TF_DEMO"
-#  schema      = "TERRAFORM"
-#}
-
-# assumes stage exists, requires a valid vpc
-
-#resource "snowflake_network_rule" "rule" {
-#  name       = "internal_stage_rule"
-#  database   = "TF_DEMO"
-#  schema     = "TERRAFORM"
-#  comment    = "A network rule for internal stage."
-#  type       = "AWSVPCEID"
-#  mode       = "INTERNAL_STAGE"
-#  value_list = ["vpce-xxxxxx"]
-#}
